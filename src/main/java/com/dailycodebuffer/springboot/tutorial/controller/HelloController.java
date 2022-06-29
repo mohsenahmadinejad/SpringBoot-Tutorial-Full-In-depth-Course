6e4b0b5b856356ad4e49fdd7288ec89339162a44
mohsen ahmadi <mohsen@ahmadi.com>
2022-06-30 01:20:21 +0430

    first commit

---
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.dailycodebuffer.springboot.tutorial.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//    @RequestMapping(value="/", method = RequestMethod.GET)
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Welcome to daily code buffer";
+    }
+
+}
